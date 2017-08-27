55951f090a8ebce6943783795e38b659a158ab02
davipmanh <dinhmanhvivu@gmail.com>
2017-08-27 21:32:49 +0700

    Slave_Full

--- Slave_12_08_17_RS485_Sensor_LCD_Joystick/Slave_12_08_17_RS485_Sensor_LCD_Joystick.ino
+++ Slave_12_08_17_RS485_Sensor_LCD_Joystick/Slave_12_08_17_RS485_Sensor_LCD_Joystick.ino
@@ -1,6 +1,6 @@
 /* PROJECT: SLAVE/RS485/MODBUS RTU
    AUTHOR: DAVIP MANH
-   DADE: 15/08/2017
+   DADE: 20/08/2017
    VERSION: 1.0
 */
 
@@ -12,7 +12,7 @@
 Modbus slave(ID, 0, 8); // (uint8_t u8id, uint8_t u8serno, uint8_t u8txenpin); pin 8: control RS485 module
 
 // Data array for modbus network sharing
-#define NumberData  7
+#define NumberData  16
 uint16_t au16data[NumberData];
 //***************** End RS485 *****************//
 
@@ -22,7 +22,14 @@ uint16_t au16data[NumberData];
 #define DHT1PIN   A0    // Pin name for Sensor 1
 #define DHT2PIN   A1    // Pin name for Sensor 2
 #define DHTTYPE   DHT21 // DHT 21 (AM2301)
-#define timeread  100  // time read data of sensors
+#define timeread  100   // time read data of sensors
+
+#define delta_t    1   // delta value for temperature calibration
+#define delta_h    0.1   // delta value for humidity calibration
+#define delta_rl1  1000  // delta value for timer relay 1 calibration
+#define delta_rl2  1000  // delta value for timer relay 2 calibration
+#define delta_up   0.5   // delta value for upper calibration
+#define delta_low  0.5   // delta value for lower calibration
 
 DHT dht1(DHT1PIN, DHTTYPE);  //Declare for Sensor 1
 DHT dht2(DHT2PIN, DHTTYPE);  //Declare for Sensor 2
@@ -31,17 +38,18 @@ float h1, t1, f1, h2, t2, f2;
 signed int _h1, _t1, _f1, _h2, _t2, _f2; // Convert from float to int
 unsigned long tempms;
 int sensor;
-float dt1 = 0.0, dh1 =  0.0, dt2 =  0.0, dh2 =  0.0, up1 = 40, low1 = 25, up2 = 40, low2 = 25;
-unsigned int time1 = 5000, time2 = 5000;
+float dt1 = 0.0, dh1 =  0.0, dt2 =  0.0, dh2 =  0.0, up1 = 40, low1 = 25, up2 = 80, low2 = 50;
+unsigned int time1 = 10000, time2 = 10000;
 //***************** End Sensor DHT *****************//
 
 //***************** Begin LCD *****************//
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-unsigned long delayclr, out;
+unsigned long delayclr, out, timeChange, timeBlink;
 int change = -1;
 int counter;
+bool edit = 0;
 //***************** End LCD *****************//
 
 //***************** Begin Joystick *****************//
@@ -62,24 +70,23 @@ int mode = 0, dp = 0, dp1 = 1, dp2 = 1, dp3 = 1;
 //***************** End Joystick *****************//
 
 //***************** Begin Declare Relay *****************//
-#define RL1      8
-#define RL2      9
-#define RL3      10
-#define RL4      11
+#define RL1      9
+#define RL2      10
+#define RL3      11
+#define RL4      12
 
-unsigned long timeOnRL;
-
+unsigned long timeOnRL1, timeOnRL2, timeOnRL3, timeOnRL4;
+byte flag = 0;
 //***************** End Declare Relay *****************//
 
 //***************** Declare Subroutines *****************//
 void io_poll();
 void clearlcd(int t);
 void outmode(int t);
-void displaydp(String text1, String text2);
+void displaydp(String text1, String text2, float value);
 int readjs();
-void blinkCursor();
-void check(int value);
-void UpdateValue(int x, float t);
+void blinkCursor(int x, int y);
+void UpdateValue(float x, float t);
 
 //***************** Declare Subroutines *****************//
 
@@ -104,6 +111,8 @@ void setup() {
   //delay(1000);
   lcd.clear();
   delayclr = millis();
+  timeChange = millis();
+  timeBlink = millis();
 
   //Setup Relay
   pinMode(RL1, OUTPUT);
@@ -111,23 +120,37 @@ void setup() {
   pinMode(RL3, OUTPUT);
   pinMode(RL4, OUTPUT);
 
-  digitalWrite(RL1, LOW);
-  digitalWrite(RL2, LOW);
-  digitalWrite(RL3, LOW);
-  digitalWrite(RL4, LOW);
+  digitalWrite(RL1, HIGH);
+  digitalWrite(RL2, HIGH);
+  digitalWrite(RL3, HIGH);
+  digitalWrite(RL4, HIGH);
 
-  timeOnRL = millis();
+  timeOnRL1 = millis();
+  timeOnRL2 = millis();
+  timeOnRL3 = millis();
+  timeOnRL4 = millis();
 }
 
 void loop() {
   if (readjs() == Right || readjs() == Left)
   {
-    delay(220);
-    seen = !seen;
-    delay(30);
+    if (millis() > timeChange + 400)
+    {
+      seen ^= 1;
+      lcd.clear();
+      timeChange = millis();
+    }
   }
 
-  if (readjs() == Press) mode = 1;
+  if (readjs() == Press)
+  {
+    if (millis() > timeChange + 400)
+    {
+      mode ^= 1;
+      lcd.clear();
+      timeChange = millis();
+    }
+  }
   if (mode == 0)
   {
     if (millis() > tempms)
@@ -135,12 +158,12 @@ void loop() {
       //Read Sensor 1
       h1 = dht1.readHumidity();
       t1 = dht1.readTemperature();
-      f1 = dht1.readTemperature(true);
+      //f1 = dht1.readTemperature(true);
 
       //Read Sensor 1
       h2 = dht2.readHumidity();
       t2 = dht2.readTemperature();
-      f2 = dht2.readTemperature(true);
+      //f2 = dht2.readTemperature(true);
 
       //Check if any reads failed and exit early (to try again).
       if (isnan(h1) || isnan(t1) || isnan(f1))  bitWrite( au16data[6], 0, 1);
@@ -149,18 +172,18 @@ void loop() {
       else bitWrite( au16data[6], 1, 0);
 
       // Convert value to int
-      _h1 = h1 * 100;
-      _t1 = t1 * 100;
-      _f1 = f1 * 100;
+      _h1 = (h1 + dh1) * 100;
+      _t1 = (t1 + dt1) * 100;
+      //_f1 = f1 * 100;
 
-      _h2 = h2 * 100;
-      _t2 = t2 * 100;
-      _f2 = f2 * 100;
+      _h2 = (h2 + dh2) * 100;
+      _t2 = (t2 + dt2) * 100;
+      //_f2 = f2 * 100;
       if (seen)
       {
         if (isnan(h1) || isnan(t1) || isnan(f1))
         {
-          lcd.setCursor(4, 0);
+          lcd.setCursor(0, 0);
           lcd.print("    Sensor 1    ");
           lcd.setCursor(0, 1);
           lcd.print("   No Device!   ");
@@ -168,14 +191,14 @@ void loop() {
         else
         {
           //clearlcd(3000);
-          lcd.setCursor(4, 0);
+          lcd.setCursor(0, 0);
           lcd.print("    Sensor 1    ");
           lcd.setCursor(0, 1);
-          lcd.print(t1);
+          lcd.print(t1 + dt1);
           lcd.write(223); // Icon 0 - 1101 1111 - 0xDF
           lcd.print("C  ");
           lcd.setCursor(9, 1);
-          lcd.print(h1);
+          lcd.print(h1 + dh1);
           lcd.print("%  ");
         }
       }
@@ -183,7 +206,7 @@ void loop() {
       {
         if (isnan(h2) || isnan(t2) || isnan(f2))
         {
-          lcd.setCursor(4, 0);
+          lcd.setCursor(0, 0);
           lcd.print("    Sensor 2    ");
           lcd.setCursor(0, 1);
           lcd.print("   No Device!   ");
@@ -191,35 +214,108 @@ void loop() {
         else
         {
           //clearlcd(3000);
-          lcd.setCursor(4, 0);
+          lcd.setCursor(0, 0);
           lcd.print("    Sensor 2    ");
           lcd.setCursor(0, 1);
-          lcd.print(t2);
+          lcd.print(t2 + dt2);
           lcd.write(223); // Icon 0 - 1101 1111 - 0xDF
           lcd.print("C  ");
           lcd.setCursor(9, 1);
-          lcd.print(h2);
+          lcd.print(h2 + dh2);
           lcd.print("%  ");
         }
       }
       tempms = millis() + timeread;
     }
-    if (dt1 >= up1)
+
+    if ((t1 + dt1) >= up1)
     {
-      On_RL(1, time1);
+      if (millis() > (timeOnRL1 + time1))
+      {
+        if ((t1 + dt1) >= up1)
+        {
+          digitalWrite(RL1, LOW);
+          flag |= B0001;
+          timeOnRL1 = millis();
+        }
+      }
     }
-    else if (dt1 <= low1)
+    else
     {
-      On_RL(2, time1);
+      if ((t1 + dt1) <= low1)
+      {
+        digitalWrite(RL1, HIGH);
+        flag &= B1110;
+      }
+      timeOnRL1 = millis();
     }
-    if (dt2 >= up2)
+
+    if ((t2 + dt2) >= up1)
     {
-      On_RL(3, time2);
+      if (millis() > (timeOnRL3 + time1))
+      {
+        if ((t2 + dt2) >= up1)
+        {
+          digitalWrite(RL3, LOW);
+          flag |= B0100;
+          timeOnRL3 = millis();
+        }
+      }
     }
-    else if (dt2 <= low2)
+    else
     {
-      On_RL(4, time2);
+      if ((t2 + dt2) <= low1)
+      {
+        digitalWrite(RL3, HIGH);
+        flag &= B1011;
+      }
+      timeOnRL3 = millis();
     }
+
+    if ((h1 + dh1) >= up2)
+    {
+      if (millis() > (timeOnRL2 + time2))
+      {
+        if ((h1 + dh1) >= up2)
+        {
+          digitalWrite(RL2, LOW);
+          flag |= B0010;
+          timeOnRL2 = millis();
+        }
+      }
+    }
+    else
+    {
+      if ((h1 + dh1) <= low2)
+      {
+        digitalWrite(RL2, HIGH);
+        flag &= B1101;
+      }
+      timeOnRL2 = millis();
+    }
+
+    if ((h2 + dh2) >= up2)
+    {
+      if (millis() > (timeOnRL4 + time2))
+      {
+        if ((h2 + dh2) >= up2)
+        {
+          digitalWrite(RL4, LOW);
+          flag |= B1000;
+          timeOnRL4 = millis();
+        }
+      }
+    }
+    else
+    {
+      if ((h2 + dh2) <= low2)
+      {
+        digitalWrite(RL4, HIGH);
+        flag &= B0111;
+      }
+      timeOnRL4 = millis();
+    }
+
     //Poll messages RS485
     slave.poll( au16data, NumberData );
     io_poll();
@@ -228,96 +324,248 @@ void loop() {
   {
     if (readjs() == Right)
     {
-      dp++;
+      if (millis() > timeChange + 220)
+      {
+        dp++;
+        lcd.clear();
+        timeChange = millis();
+      }
       out = millis();
       if (dp > 10) dp = 0;
     }
     else if (readjs() == Left)
     {
-      dp--;
+      if (millis() > timeChange + 210)
+      {
+        dp--;
+        lcd.clear();
+        timeChange = millis();
+      }
       out = millis();
       if (dp < 0) dp = 10;
     }
     switch (dp) {
       case 0:
-        displaydp("    Setting   ", "Delta T1 =      ");
-        lcd.setCursor(11, 1);
-        lcd.print(dt1);
-        check(0);
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dt1 += delta_t;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dt1 -= delta_t;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Delta T1 = ", dt1);
         break;
       case 1:
-        displaydp("    Setting   ", "Delta H1 =      ");
-        lcd.setCursor(11, 1);
-        lcd.print(dh1);
-        check(1);
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dh1 += delta_h;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dh1 -= delta_h;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Delta H1 = ", dh1);
         break;
       case 2:
-        displaydp("    Setting   ", "Delta T2 =      ");
-        lcd.setCursor(11, 1);
-        lcd.print(dt2);
-        check(2);
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dt2 += delta_t;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dt2 -= delta_t;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Delta T2 = ", dt2);
         break;
       case 3:
-        displaydp("    Setting   ", "Delta H2 =      ");
-        lcd.setCursor(11, 1);
-        lcd.print(dh2);
-        check(3);
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dh2 += delta_h;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            dh2 -= delta_h;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Delta H2 = ", dh2);
         break;
       case 4:
-        displaydp("    Setting   ", "TimeRL12 =      ");
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            time1 += delta_rl1;
+            lcd.clear();
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            time1 -= delta_rl1;
+            lcd.clear();
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "TimeRL T = ", time1 / 1000);
         break;
       case 5:
-        displaydp("    Setting   ", "TimeRL34 =      ");
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            time2 += delta_rl2;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            time2 -= delta_rl2;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "TimeRL H = ", time2 / 1000);
         break;
       case 6:
-        displaydp("    Setting   ", "Upper 1 <=      ");
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            up1 += delta_up;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            up1 -= delta_up;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Upper 1 <= ", up1);
         break;
       case 7:
-        displaydp("    Setting   ", "Lower 1 >=      ");
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            low1 += delta_low;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            low1 -= delta_low;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Lower 1 >= ", low1);
         break;
       case 8:
-        displaydp("    Setting   ", "Upper 2 <=      ");
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            up2 += delta_up;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            up2 -= delta_up;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Upper 2 <= ", up2);
         break;
       case 9:
-        displaydp("    Setting   ", "Lower 2 >=      ");
+        if (readjs() == Up)
+        {
+          if (millis() > timeChange + 200)
+          {
+            low2 += delta_low;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        else if (readjs() == Down)
+        {
+          if (millis() > timeChange + 200)
+          {
+            low2 -= delta_low;
+            timeChange = millis();
+            out = millis();
+          }
+        }
+        displaydp("    Setting   ", "Lower 2 >= ", low2);
         break;
       case 10:
-        displaydp("    Setting   ", "  Exit Setting  ");
-        break;
-      default:
-        break;
-    }
-    switch (change)
-    {
-      case 0:
-        UpdateValue(dt1, 0.1);
-        break;
-      case 1:
-        UpdateValue(dh1, 0.1);
-        break;
-      case 2:
-        UpdateValue(dt2, 0.1);
-        break;
-      case 3:
-        UpdateValue(dh2, 0.1);
-        break;
-      case 4:
-        UpdateValue(time1, 1);
-        break;
-      case 5:
-        UpdateValue(time2, 1);
-        break;
-      case 6:
-        UpdateValue(up1, 0.5);
-        break;
-      case 7:
-        UpdateValue(low1, 0.5);
-        break;
-      case 8:
-        UpdateValue(up2, 0.5);
-        break;
-      case 9:
-        UpdateValue(low2, 0.5);
+        lcd.setCursor(0, 0);
+        lcd.print(" Information ");
+        lcd.setCursor(14, 0);
+        if (dp < 10)
+          lcd.print(" ");
+        lcd.print(dp);
+        lcd.setCursor(0, 1);
+        lcd.print(" 090.294.68.24  ");
         break;
       default:
         break;
@@ -336,14 +584,44 @@ void io_poll() {
   //  analogWrite( 10, au16data[2] );
 
   // read analog inputs - Maximum value is 2^15 -1 = 32767
-  au16data[0] = _h1;
-  au16data[1] = _t1;
-  au16data[2] = _f1;
+  au16data[0] = _t1;
+  au16data[1] = _h1;
+  //au16data[2] = _f1;
 
+  au16data[2] = _t2;
   au16data[3] = _h2;
-  au16data[4] = _t2;
-  au16data[5] = _f2;
-
+  //  bitWrite( au16data[4], 0, digitalRead(RL1));
+  //  bitWrite( au16data[4], 1, digitalRead(RL2));
+  //  bitWrite( au16data[4], 2, digitalRead(RL3));
+  //  bitWrite( au16data[4], 3, digitalRead(RL4));
+  au16data[4] = flag;
+  bool x = au16data[5];
+  if (x)
+  {
+    dt1 = au16data[6];
+    dh1 = au16data[7];
+    dt2 = au16data[8];
+    dh2 = au16data[9];
+    time1 = au16data[10];
+    time2 = au16data[11];
+    up1 = au16data[12];
+    low1 = au16data[13];
+    up2 = au16data[14];
+    low2 = au16data[15];
+  }
+  else
+  {
+    au16data[6] = dt1;
+    au16data[7] = dh1 ;
+    au16data[8] = dt2 ;
+    au16data[9] = dh2;
+    au16data[10] = time1 ;
+    au16data[11] = time2;
+    au16data[12] = up1;
+    au16data[13] = low1;
+    au16data[14] = up2;
+    au16data[15] = low2;
+  }
   // diagnose communication
   //au16data[6] = slave.getInCnt();
   //au16data[7] = slave.getOutCnt();
@@ -367,14 +645,18 @@ void outmode(int t)
   }
 }
 
-void displaydp(String text1, String text2)
+void displaydp(String text1, String text2, float value)
 {
   lcd.setCursor(0, 0);
   lcd.print(text1);
-  lcd.setCursor(15, 0);
+  lcd.setCursor(14, 0);
+  if (dp < 10)
+    lcd.print(" ");
   lcd.print(dp);
   lcd.setCursor(0, 1);
   lcd.print(text2);
+  lcd.setCursor(11, 1);
+  lcd.print(value);
   outmode(10000);
 }
 
@@ -391,84 +673,20 @@ int readjs()
   else return 0;
 }
 
-void blinkCursor()
+void blinkCursor(int x, int y)
 {
-  lcd.noCursor();
-  delay(200);
-  // Turn on the cursor:
-  lcd.cursor();
-  delay(200);
+  if (millis() > timeBlink)
+  {
+    lcd.setCursor(x, y);
+    lcd.cursor();
+    timeBlink = millis() + 500;
+  }
+  else
+    lcd.noCursor();
 }
 
-void check(int value)
+void UpdateValue(float x, float t)
 {
-  if (readjs() == Press)
-    change = value;
-  if (change > 0)
-  {
-    lcd.setCursor(11, 1);
-    blinkCursor();
-  }
-}
-
-void UpdateValue(int x, float t)
-{
-  if (readjs() == Up)
-  {
-    out = millis();
-    x += t;
-  }
-  else if (readjs() == Down)
-  {
-    out = millis();
-    x -= t;
-  }
-}
-void OnRL(int rl, int t)
-{
-  if (rl = 1)
-  {
-    if (millis() > timeOnRL1 + time1)
-    {
-      if (t1 > up1)
-      {
-        digitalWrite(RL1, HIGH);
-        timeOnRL1 = millis();
-      }
-      else digitalWrite(RL1, LOW);
-    }
-  }
-  else if (rl = 2)
-  {
-    if (millis() > timeOnRL2 + time2)
-    {
-      if (h1 > up2)
-      {
-        digitalWrite(RL2; , HIGH);
-        timeOnRL2 = millis();
-      }
-    }
-  }
-  else if (rl = 3)
-  {
-    if (millis() > timeOnRL3 + time3)
-    {
-      if (t2 > up1)
-      {
-        digitalWrite(RL1, HIGH);
-        timeOnRL1 = millis();
-      }
-    }
-  }
-  else if (rl = 2)
-  {
-    if (millis() > timeOnRL2 + time2)
-    {
-      if (h2 > up2)
-      {
-        digitalWrite(RL2; , HIGH);
-        timeOnRL2 = millis();
-      }
-    }
-  }
+  if (readjs() == Up) x += t;
+  else if (readjs() == Down)  x -= t;
 }
